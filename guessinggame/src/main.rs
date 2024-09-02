d461b2878d579fdf8b5aac92c3fe809a1b2c5a48
Sanskar-12 <sanskarv2004@gmail.com>
2024-09-02 19:46:41 +0530

    guessing game

--- guessinggame/src/main.rs
+++ guessinggame/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -15,5 +16,12 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Error");
 
-    println!("Your guess was {guess}");
+    let guess:u32=guess.trim().parse().expect("Failed to parse"); //shadowing the variable
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("You Won!"),
+        Ordering::Greater => println!("Number Guessed is Greater, Please choose Lesser Number!"),
+        Ordering::Less => println!("Number Guessed is Lesser, Please choose Greater Number!")
+    }
+
 }
