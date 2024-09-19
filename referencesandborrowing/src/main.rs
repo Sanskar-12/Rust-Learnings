67cb69ba7d1b7f9bef29ac99826e81bf61b84c27
Sanskar-12 <sanskarv2004@gmail.com>
2024-09-19 22:10:21 +0530

    cannot have multiple mutable references

--- referencesandborrowing/src/main.rs
+++ referencesandborrowing/src/main.rs
@@ -13,14 +13,35 @@
 // }
 
 // ----------------------------------------mutable references ----------------------//
-fn main() {
-    let mut s=String::from("Hello world");
-    let len = calculate_length(&mut s); 
-    println!("length of {s} is {len}")
-}
+// fn main() {
+//     let mut s=String::from("Hello world");
+//     let len = calculate_length(&mut s); 
+//     println!("length of {s} is {len}")
+// }
 
-fn calculate_length(s1:&mut String) -> usize { // here s1 is a mutable reference variable which is pointing to s and s is pointing to the heap 
-    s1.push_str("Sanskar"); // we can mutate the value because i have made the reference as mutable
-    let res = s1.len();
-    res
+// fn calculate_length(s1:&mut String) -> usize { // here s1 is a mutable reference variable which is pointing to s and s is pointing to the heap 
+//     s1.push_str("Sanskar"); // we can mutate the value because i have made the reference as mutable
+//     let res = s1.len();
+//     res
+// }
+
+
+// ---------------------------------cannot have nultiple mutable refereces---------------//
+fn main() {
+    // let mut s=String::from("Hello");
+    // let s1=&mut s;
+    // let s2=&mut s; // it will give error because you cannot have multiple mutable references in the same scope
+
+    // s2.push_str("Hello");
+
+    // println!("{s1}")
+
+    // {
+    //     let r1=&mut s; // this will not give error because both have different scope in which they are defined
+    // }
+
+    // let r2=&mut s;
+    // r2.push_str("Hello");
+
+    // println!("{r2}");
 }
