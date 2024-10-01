ea77edbaf87ec9141dcb4624fe0e6e51eb69a632
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-01 23:35:10 +0530

    enums done

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -24,19 +24,37 @@
 
 // ------------------ we can directly attach the data with the enum------------//
 
-#[derive(Debug)]
-enum IpAddrKind {
-    V4(String),
-    V6(u8,u8,u8,u8) // this is tuple
-}
+// #[derive(Debug)]
+// enum IpAddrKind {
+//     V4(String),
+//     V6(u8,u8,u8,u8) // this is tuple
+// }
 
-fn route(ip:IpAddrKind) {
-    println!("routing to ip address {ip:?}")
-}
+// fn route(ip:IpAddrKind) {
+//     println!("routing to ip address {ip:?}")
+// }
+
+// fn main() {
+//     route(IpAddrKind::V4(String::from("1.0.0.0")));
+//     route(IpAddrKind::V6(127,0,0,1));
+// }
+
+// ---------Option enum---------------------------//
+
+// Rust doesnt have null value so Option enum has made which has None and Some value
+
+// enum Option<T> { // this way Option enum is made
+//     None,
+//     Some(T)
+// }
 
 fn main() {
-    route(IpAddrKind::V4(String::from("1.0.0.0")));
-    route(IpAddrKind::V6(127,0,0,1));
-}
+    let enum1: Option<i32>=Option::Some(1); // if the value contains then we use Some
+    let enum2: Option<i32>=Option::None; // if the value doesnt contains or it is Null then we use None
 
+    let x=10;
+
+    let res=x+enum1.unwrap_or(0); // we can unwrap the enum1 to take the number from the Option::Some()
+    println!("{res}");
+}
 
